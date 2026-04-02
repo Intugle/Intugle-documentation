6cd221ad59df575e0d2a6f329e4ca9588587c486
rabindra-neupane-intugle <rabindra@intugle.ai>
2026-04-02 13:55:56 +0530

    added BOM in architecture page

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,6 +3,41 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '5ff'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', '5ba'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', 'a2b'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', 'c3c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', '156'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '88c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', '000'),
+    exact: true
+  },
+  {
     path: '/blog',
     component: ComponentCreator('/blog', 'b2f'),
     exact: true
@@ -84,15 +119,15 @@ export default [
   },
   {
     path: '/',
-    component: ComponentCreator('/', '731'),
+    component: ComponentCreator('/', '372'),
     routes: [
       {
         path: '/',
-        component: ComponentCreator('/', '4ef'),
+        component: ComponentCreator('/', '8ad'),
         routes: [
           {
             path: '/',
-            component: ComponentCreator('/', 'a6f'),
+            component: ComponentCreator('/', '5f3'),
             routes: [
               {
                 path: '/Connections/adls',
@@ -127,6 +162,24 @@ export default [
               {
                 path: '/FAQs/',
                 component: ComponentCreator('/FAQs/', '562'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/FAQs/adls',
+                component: ComponentCreator('/FAQs/adls', '94f'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/FAQs/azureblob',
+                component: ComponentCreator('/FAQs/azureblob', 'e47'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/FAQs/bigquery',
+                component: ComponentCreator('/FAQs/bigquery', '834'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
--- docs/Intro.md
+++ docs/Intro.md
@@ -4,6 +4,28 @@ sidebar_position: 1
 ---
 
 <!-- ![Intugle Architecture](https://intugle.ai/intugle-logo.svg) -->
+# 1. Architecture Diagram
 
 ![Alt text](/img/Intugle-v3-architecture-Databricks.png)
 
+
+# 2. System Requirements
+
+|   Category    | Service                           |   SKU                                 |   Purpose                                    |
+|---------------|-----------------------------------|---------------------------------------|----------------------------------------------|
+|   Compute     |   Kubernetes (AKS)                | 5 X Standard_D8lds_v5                 | Run application microservices                |
+|   Compute     |   Databricks                      | 1 Small WH, 1 General Compute         | Compute Engine                               |
+|   Compute     |   Virtual machine                 | Standard D4lds                        | Jump Host                                    |
+|   Containers  |   Container registry              | 9 microservice images                 | Store container images                       |
+|   LLM         |   Foundry/Foundry Project         | GPT 4X, GPT 5X, Text Embedding Ada    | Deploy LLM models                            |
+|   Storage     |   Storage account                 | ADLS V2                               | Data Lake                                    |
+|   Storage     |   Storage account                 | NFS - 1 TB                            | Persistance for microservices                |
+
+
+# 3. Third party Tools
+
+|   Category  | Service |   SKU      |   Purpose |
+|-----------------|---------------------------------|----------|----------------------------------------------------|
+| Authentication    | WorkOS    | WorkOS            | Authentication Identity Providor                              |
+| Monitoring        | Datadog   | Datadog           | Log Analytics and Application Monitoring                               |
+| Monitoring        | Langfuse  | Langfuse          | LLM Agents Analytics and monitoring                               |
